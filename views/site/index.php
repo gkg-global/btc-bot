686fa8f10599915378231c37931eedf6fcacc1e7
Ubuntu <ubuntu@ip-172-31-25-100.eu-central-1.compute.internal>
2018-02-24 20:16:22 +0000

    project index

---
+++ views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'BTC Bot';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hi there! I'm BTC bot!</h1>
+
+        <p class="lead">Your personal assistant.</p>
+
+    </div>
+
+</div>
